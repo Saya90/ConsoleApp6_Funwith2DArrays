ae5b8f6580cf04839d92443882217d41763b7085
bshad <bshad@DESKTOP-TNN86GQ>
2025-06-24 20:46:59 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp6_Funwith2DArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
