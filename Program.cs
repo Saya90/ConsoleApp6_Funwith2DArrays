1fb071d8f441a73e2e78810c18e4c97d4e7771fd
Saya90 <92330097+Saya90@users.noreply.github.com>
2025-07-08 18:59:58 +0200

    ask user to choose numbers of field on the board

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi, Tell me how many fields do you want your board to have? Please type only numbers");
+            Console.WriteLine("Columns:");
+            int column = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Rows:");
+            int row = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You typed  " + column + " , "  + row);
+
+
+            // int[,] board = new int[3,3;
+
+            // board [0,0] =  
+            // Console.WriteLine("Hello, World!");
         }
     }
 }
